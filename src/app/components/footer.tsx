9d82839493537af541efb2727d7a4e469f2306b4
Jgabriel <megagbl2323@gmail.com>
2025-01-18 11:46:31 -0400

    refat foooter

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,32 +1,38 @@
-import Image from "next/image"
+import Image from "next/image";
 
-export default function Footer(){
-    return (
-        <>
-        <div className="w-full flex-col p-6 pb-36 gap-20 bg-black">
-            <div>
-          <div className="flex flex-col gap-2 z-10 relative">
-            <h1>SOCI<span className="text-fuchsia-700">AL</span></h1>
-            <div className="flex gap-2 z-20">
-              <a href="https://github.com/gabrieldevtjs" target="_blank" rel="noopener noreferrer" className="">
-                <Image src="/icons/github.png" alt="GitHub" width={30} height={20} className="z-20" />
-              </a>
-              <a href="https://www.linkedin.com/in/jos%C3%A9-gabriel-8901a7344/" target="_blank" rel="noopener noreferrer">
-                <Image src="/icons/linkedin.png" alt="LinkedIn" width={30} height={20} />
-              </a>
-              <a href="https://www.instagram.com/gabrieltj.dev?igsh=NndhZHJjMDF0MHNr&utm_source=qr" target="_blank" rel="noopener noreferrer">
-                <Image src="/icons/instagram.png" alt="Instagram" width={30} height={20} />
-              </a>
-            </div>
-            <div className="flex flex-col gap-3 pt-7 pb-12 border-b border-gray-400">
-            <h1>JOSÉ GAB<span className="text-fuchsia-700">RIEL</span></h1>
-            <p className="text-xs">
-            Um desenvolvedor web focado em frontend que constrói o frontend de sites e aplicativos da web que leva ao sucesso do produto geral
-            </p>
-          </div>
-          </div>
-          </div>
+export default function Footer() {
+  return (
+    <footer className="w-full flex flex-col lg:flex-row justify-between items-center bg-gray-950 px-12 py-8 text-gray-300">
+      {/* Seção de Direitos Autorais */}
+      <div className="mb-6 lg:mb-0">
+        <p className="text-sm text-gray-500">
+          &copy; 2025 Gabriel. Todos os direitos reservados.
+        </p>
+      </div>
+
+      {/* Seção de Redes Sociais */}
+      <div>
+        <div className="flex gap-4 items-center justify-center">
+          <a href="https://github.com/gabrieldevtjs" target="_blank" rel="noopener noreferrer">
+            <Image src="/icons/github.png" alt="GitHub" width={30} height={20} />
+          </a>
+
+          <a href="https://www.linkedin.com/in/jos%C3%A9-gabriel-8901a7344/" target="_blank" rel="noopener noreferrer">
+            <Image src="/icons/linkedin.png" alt="LinkedIn" width={30} height={20} />
+          </a>
+
+          <a href="https://www.instagram.com/gabrieltj.dev?igsh=NndhZHJjMDF0MHNr&utm_source=qr" target="_blank" rel="noopener noreferrer">
+            <Image src="/icons/instagram.png" alt="Instagram" width={30} height={20} />
+          </a>
         </div>
-      </>
-    )
+      </div>
+    </footer>
+  );
 }
+
+
+{/* <div className="">
+<p className="text-sm text-gray-500">
+  &copy; 2025 Gabriel. Todos os direitos reservados.
+</p>
+</div> */}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,28 +20,31 @@ export default function Home() {
     <div className="w-full min-h-screen font-Kanit">
       <Header />
       <section className="w-full pt-16 2xl:px-8">
-        <div className="w-full 2xl:flex 2xl:pb-20">
+        <div className="w-full 2xl:flex 2xl:pb-20  rounded-3xl">
         <div id="homee" className="w-full flex flex-col justify-around items-center pt-40 pb-32 gap-10 sm:pt-52 2xl:w-1/2" data-aos='fade-down'>
           <div className="text-center gap-5 flex flex-col">
             <div className="">
-              <h1 className="text-4xl sm:text-6xl text-indigo-700 font-ka">FULL STACK</h1>
-              <h2 className="text-3xl sm:text-5xl">DEVELOPER</h2>
+              <h1 className="text-5xl sm:text-6xl text-indigo-700">FULL STACK</h1>
+              <h2 className="text-4xl sm:text-5xl">DEVELOPER</h2>
             </div>
 
-            <div className="w-full px-1 flex items-center justify-center">
-              <span className="border-2 border-indigo-700 px-2 py-5 rounded-2xl">
-                      <Image
-                      src="/icons/circulo-preto.png"
-                      alt="Descrição da imagem"
-                      width={10}
-                      height={10} 
-                      className="animate-bounce"
-                      />
-              </span>
-            </div>
+            <div className="flex gap-2 items-center justify-center">
+            <a href="https://github.com/gabrieldevtjs" target="_blank">
+              <Image src="/icons/github.png" alt="GitHub" width={30} height={20} />
+            </a>
+
+            <a href="https://www.linkedin.com/in/jos%C3%A9-gabriel-8901a7344/" target="_blank">
+              <Image src="/icons/linkedin.png" alt="LinkedIn" width={30} height={20} />
+            </a>
+
+            <a href="https://www.instagram.com/gabrieltj.dev?igsh=NndhZHJjMDF0MHNr&utm_source=qr" target="_blank">
+              <Image src="/icons/instagram.png" alt="Instagram" width={30} height={20} />
+            </a>
+            
+          </div>
           </div>
 
-          <p className="text-center px-1 font-Kanit text-1xl text-stone-700 sm:px-2 lg:px-16 lg:text-1xl">
+          <p className="text-center px-1 font-Kanit text-1xl text-stone-700 sm:px-2 lg:px-16 xl:px-44">
             Olá, me chamo <span className="text-indigo-900">Gabriel</span>, sou desenvolvedor <span className="text-indigo-900">Full Stack</span>. Trabalho com soluções <span className="text-indigo-900">web</span> que realmente
             funcionam e melhoram a experiência dos usuários. Estou sempre aprendendo novas <span className="text-indigo-900">tecnologias</span> para
             entregar projetos legais e eficientes.
@@ -62,40 +65,31 @@ export default function Home() {
             </a>
           </button>
 
-          <div className="flex gap-2">
-            <a href="https://github.com/gabrieldevtjs" target="_blank">
-              <Image src="/icons/github.png" alt="GitHub" width={30} height={20} />
-            </a>
-
-            <a href="https://www.linkedin.com/in/jos%C3%A9-gabriel-8901a7344/" target="_blank">
-              <Image src="/icons/linkedin.png" alt="LinkedIn" width={30} height={20} />
-            </a>
-
-            <a href="https://www.instagram.com/gabrieltj.dev?igsh=NndhZHJjMDF0MHNr&utm_source=qr" target="_blank">
-              <Image src="/icons/instagram.png" alt="Instagram" width={30} height={20} />
-            </a>
-          </div>
+          
           </div>
         </div>
 
         <div className="hidden 2xl:flex 2xl:w-1/2 rounded-2xl items-center justify-center flex-col" data-aos='fade-left'>
+        <div className="p-20 bg-indigo-700 rounded-3xl">
         <Image
             src="/icons/mesa.svg"
             alt="Descrição da imagem"
             width={350}
             height={40} 
         />
+        
         <span className="text-gray-500 text-sm py-5">"Programar é 100%, 90% de erro e 10% de sorte no meio!"</span>
         </div>
         </div>
+        </div>
  
-        <div className="flex flex-col lg:flex-row w-full 2xl:px-16 2xl:pt-32 2xl:pb-40" data-aos='fade-down'>
+        <div className="flex flex-col lg:flex-row w-full 2xl:px-16 2xl:pt-32 2xl:pb-40 bg-cover bg-center" data-aos='fade-down'>
         {/*sobre mim */}
-        <div id="sobre" className="pt-16 pb-16 w-full flex justify-center items-center flex-col gap-2 px-3  rounded-2xl">
+        <div id="sobre" className="pt-16 pb-16 w-full flex justify-center items-center flex-col gap-2 px-3 rounded-2xl ">
           <span className="text-4xl">SO<span className="text-indigo-700">BRE</span> MIM</span>
           <span className="border-t-4 border-indigo-700 animate-move-left-right">{"\u00A0".repeat(15)}</span>
 
-          <p className="text-center px-1 font-Kanit text-1xl sm:px-2 lg:px-16 lg:text-1xl lg:text-justify">
+          <p className="text-justify px-1 font-Kanit text-1xl sm:px-2 lg:px-16 lg:text-1xl">
           Olá, me chamo Gabriel, sou estudante de Engenharia de Software, cursando o 3º período no Instituto de Tecnologia FUCAPI. Sou apaixonado por tecnologia e estou focado no desenvolvimento web e back-end, com ênfase em Node.js e TypeScript. Meu objetivo é me especializar no desenvolvimento de aplicativos Android, criando soluções que impactem positivamente o dia a dia das pessoas. Estou sempre em busca de novos aprendizados, me esforçando para crescer profissionalmente e me tornar um desenvolvedor altamente qualificado.
         </p>
         </div>
@@ -111,29 +105,70 @@ export default function Home() {
         </div>
 
         {/*Projetos */} 
-        <div id="projetos" className="pt-16 pb-16 w-ful flex justify-center items-center flex-col gap-2 px-3 2xl:px-32" data-aos='fade-down'>
+        <div id="projetos" className="pt-16 pb-12 w-ful flex flex-col justify-center items-center gap-2 px-3 2xl:px-32" data-aos='fade-down'>
           <span className="text-4xl">PROJET<span className="text-indigo-700">OS</span></span>
           <span className="border-t-4 border-indigo-700 animate-move-left-right">{"\u00A0".repeat(15)}</span>
 
-          <p className="text-center w-auto px-1 font-Kanit text-1xl sm:px-32 lg:text-1xl lg:pb-20 lg:px-60 xl:px-96">
+          <p className="text-center w-auto px-1 font-Kanit text-1xl pb-8 sm:px-32 lg:text-1xl lg:px-60 xl:px-96">
           Aqui você encontrará alguns dos projetos pessoais que criei, cada projeto contendo seu próprio estudo de caso.
         </p>
 
+        <Image
+                      src="/icons/seta-para-baixo.png"
+                      alt="Descrição da imagem"
+                      width={30}
+                      height={40} 
+                      className="animate-bounce"
+              />
+
 
       
-        <div className="flex flex-col pt-20 gap-y-32 lg:px-24 xl:px-40">
+        <div className="flex flex-col pt-12 gap-y-32 xl:px-16 ">
+
   {/* Primeiro Card Inferior */}
-  <div className="flex flex-col items-center w-full p-2 lg:flex-row  md:px-24 lg:px-0" data-aos='fade-down'>
+  <div className="flex flex-col items-center justify-center w-full p-2 lg:flex-row lg:gap-7 xl:gap-14 md:px-24 rounded-3xl" data-aos='fade-down'>
     <div className="lg:w-1/2">
+    <div className="relative">
+
       <Image 
-        src={`/icons/image-dev-house.png`} 
+        src={`/icons/dev-house.jpg`} 
         alt="DEV HOUSE" 
-        width={370} 
+        width={320} 
         height={200} 
-        className="md:h-[140px] md:w-[140px] lg:w-[190px] lg:h-[190px] lg:ml-20 xl:w-[500px] xl:h-[500px] xl:ml-14  2xl:w-[900px] 2xl:h-[500px] 2xl:ml-4" 
+        className="w-[360px] h-[200px] sm:w-[500px] sm:h-[280px] md:h-[320px] md:w-[540px] lg:w-[450px] lg:h-[240px] xl:w-[480px] xl:h-[280px]" 
       />
+      </div>
     </div>
-    <div className="lg:w-1/2 flex-col lg:items-center lg:justify-center" data-aos='fade-down'>
+    <div className="lg:w-1/2 flex-col lg:items-center lg:justify-center " data-aos='fade-down'>
+      <div className="flex flex-col items-center gap-2 px-2">
+        <span className="text-2xl xl:text-3xl">DEV H<span className="text-blue-500">OU</span>SE</span>
+        <p className="text-justify xl:text-sm text-stone-700">
+          "O TaskMaster é uma aplicação web criada para simplificar a organização de tarefas e aumentar a produtividade de equipes e indivíduos. Com uma interface intuitiva e responsiva, o sistema permite que usuários criem, editem e organizem tarefas de forma eficiente, priorizando as mais importantes e garantindo que nenhum prazo seja perdido."
+        </p>
+        <StackCard stacks={['Nextjs', 'React', 'Tailwind']} />
+        <button className="mt-4 px-6 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600">
+        <a href="https://dev-house-gilt.vercel.app/" target="_blank">Saiba Mais</a>
+        </button>
+      </div>
+    </div>
+  </div>
+  
+      {/* Segundo Card Inferior */}
+  
+      <div className="flex flex-col items-center justify-center w-full p-2 lg:flex-row lg:gap-7 xl:gap-14 md:px-24 rounded-3xl" data-aos='fade-down'>
+    <div className="lg:w-1/2">
+    <div className="relative">
+
+      <Image 
+        src={`/icons/login.jpg`} 
+        alt="DEV HOUSE" 
+        width={320} 
+        height={200} 
+        className="w-[360px] h-[200px] sm:w-[500px] sm:h-[280px] md:h-[320px] md:w-[540px] lg:w-[450px] lg:h-[240px] xl:w-[460px] xl:h-[260px]" 
+      />
+      </div>
+    </div>
+    <div className="lg:w-1/2 flex-col lg:items-center lg:justify-center " data-aos='fade-down'>
       <div className="flex flex-col items-center gap-2 px-2">
         <span className="text-2xl xl:text-3xl">DEV H<span className="text-blue-500">OU</span>SE</span>
         <p className="text-justify xl:text-sm text-stone-700">
@@ -147,18 +182,21 @@ export default function Home() {
     </div>
   </div>
 
-    {/* Segundo Card Inferior */}
-  <div className="flex flex-col items-center w-full p-2 lg:flex-row gap-8 md:px-24 lg:px-0" data-aos='fade-down'>
+{/* Terceiro Card Inferior */}
+<div className="flex flex-col items-center justify-center w-full p-2 lg:flex-row lg:gap-7 xl:gap-14 md:px-24 rounded-3xl" data-aos='fade-down'>
     <div className="lg:w-1/2">
+    <div className="relative">
+
       <Image 
-        src={`/icons/image-dev-house.png`} 
+        src={`/icons/apiazul.jpg`} 
         alt="DEV HOUSE" 
-        width={370} 
+        width={320} 
         height={200} 
-        className="md:h-[140px] md:w-[140px] lg:w-[190px] lg:h-[190px] lg:ml-20 xl:w-[500px] xl:h-[500px] xl:ml-14  2xl:w-[10000px] 2xl:h-[600px] 2xl:ml-4" 
+        className="w-[360px] h-[200px] sm:w-[500px] sm:h-[280px] md:h-[320px] md:w-[540px] lg:w-[450px] lg:h-[240px] xl:w-[480px] xl:h-[280px]" 
       />
+      </div>
     </div>
-    <div className="lg:w-1/2 flex-col lg:items-center lg:justify-center" data-aos='fade-down'>
+    <div className="lg:w-1/2 flex-col lg:items-center lg:justify-center " data-aos='fade-down'>
       <div className="flex flex-col items-center gap-2 px-2">
         <span className="text-2xl xl:text-3xl">DEV H<span className="text-blue-500">OU</span>SE</span>
         <p className="text-justify xl:text-sm text-stone-700">
@@ -172,53 +210,29 @@ export default function Home() {
     </div>
   </div>
 
-
-
-  {/* Terceiro Card Inferior */}
-  <div className="flex flex-col items-center w-full p-2 lg:flex-row gap-8 md:px-24 lg:px-0" data-aos='fade-down'>
+{/* Quarto Card Inferior */}
+<div className="flex flex-col items-center justify-center w-full p-2 lg:flex-row lg:gap-7 xl:gap-14 md:px-24 rounded-3xl" data-aos='fade-down'>
     <div className="lg:w-1/2">
+    <div className="relative">
+
       <Image 
-        src={`/icons/api.png`} 
-        alt="ECCOMERCE API" 
-        width={140} 
-        height={140} 
-        className="md:h-[140px] md:w-[140px] lg:w-[190px] lg:h-[190px] xl:w-[200px] xl:h-[200px] xl:ml-52" 
+        src={`/icons/apiazul.jpg`} 
+        alt="DEV HOUSE" 
+        width={320} 
+        height={200} 
+        className="w-[360px] h-[200px] sm:w-[500px] sm:h-[280px] md:h-[320px] md:w-[540px] lg:w-[450px] lg:h-[240px] xl:w-[480px] xl:h-[280px]" 
       />
-    </div>
-    <div className="lg:w-1/2 flex-col lg:items-center lg:justify-center">
-      <div className="flex flex-col items-center gap-2 px-2">
-        <span className="text-2xl xl:text-3xl">ECCOMERCE API</span>
-        <p className="text-justify xl:text-sm text-stone-700">
-          "Estou me desafiando a construir uma API do zero usando apenas Express, TypeScript e Node.js. O objetivo é explorar boas práticas de desenvolvimento, como estruturação de rotas e tratamento de erros, enquanto aprofundo meu conhecimento em back-end com essas tecnologias."
-        </p>
-        <StackCard stacks={['Nodejs', 'Typescript', 'Express']} />
-        <button className="mt-4 px-6 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600">
-        <a href="https://github.com/gabrieldevtjs/eccommerce-api" target="_blank">Saiba Mais</a>
-        </button>
       </div>
     </div>
-  </div>
-
-  {/* Quarto Card Inferior */}
-  <div className="flex flex-col items-center w-full p-2 lg:flex-row gap-8 md:px-24 lg:px-0" data-aos='fade-down'>
-    <div className="lg:w-1/2">
-      <Image 
-        src={`/icons/api.png`} 
-        alt="TASK LIST" 
-        width={140} 
-        height={140} 
-        className="md:h-[140px] md:w-[140px] lg:w-[190px] lg:h-[190px] xl:w-[200px] xl:h-[200px] xl:ml-52" 
-      />
-    </div>
-    <div className="lg:w-1/2 flex-col lg:items-center lg:justify-center">
+    <div className="lg:w-1/2 flex-col lg:items-center lg:justify-center " data-aos='fade-down'>
       <div className="flex flex-col items-center gap-2 px-2">
-        <span className="text-2xl xl:text-3xl">TASK LIST</span>
+        <span className="text-2xl xl:text-3xl">DEV H<span className="text-blue-500">OU</span>SE</span>
         <p className="text-justify xl:text-sm text-stone-700">
           "O TaskMaster é uma aplicação web criada para simplificar a organização de tarefas e aumentar a produtividade de equipes e indivíduos. Com uma interface intuitiva e responsiva, o sistema permite que usuários criem, editem e organizem tarefas de forma eficiente, priorizando as mais importantes e garantindo que nenhum prazo seja perdido."
         </p>
-        <StackCard stacks={['Nestjs', 'Typescript', 'Express', 'SqLite', 'PrismaORM', 'Jwt']} />
+        <StackCard stacks={['Nextjs', 'React', 'Tailwind']} />
         <button className="mt-4 px-6 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600">
-        <a href="https://github.com/gabrieldevtjs/task-list" target="_blank">SAIBA MAIS</a>   
+        <a href="https://dev-house-gilt.vercel.app/" target="_blank">Saiba Mais</a>
         </button>
       </div>
     </div>
@@ -228,38 +242,32 @@ export default function Home() {
 
         {/*contato */}
        
-        <div className="flex flex-col lg:flex-row w-full 2xl:px-16 2xl:pt-32 2xl:pb-40" data-aos='fade-down'>
-        {/*sobre mim */}
-        <div id="contato" className="pt-16 pb-16 w-full flex justify-center items-center flex-col gap-2 px-3  rounded-2xl">
-          <span className="text-4xl">CONT<span className="text-indigo-700">ATO</span></span>
-          <span className="border-t-4 border-indigo-700 animate-move-left-right">{"\u00A0".repeat(15)}</span>
+        <div className="flex flex-col lg:flex-row w-full 2xl:px-16 2xl:pt-32 2xl:pb-40 mb-32 sm:hidden" data-aos='fade-down'>
+  {/* Sobre mim */}
+  <div id="contato" className="pt-16 pb-16 w-full flex justify-center items-center flex-col gap-5 px-3 rounded-2xl">
+    <span className="text-4xl">CONT<span className="text-indigo-700">ATO</span></span>
+    <span className="border-t-4 border-indigo-700 animate-move-left-right">{"\u00A0".repeat(15)}</span>
 
-          <p className="text-center px-1 font-Kanit text-1xl sm:px-2 lg:px-16 lg:text-1xl lg:text-justify">
-          Estou aberto a oportunidades de emprego nas quais posso contribuir, aprender e crescer. Se você tiver uma boa oportunidade que corresponda às minhas habilidades e experiência, não hesite em entrar em contato comigo.
-        </p>
-        <Image
-                      src="/icons/seta-para-baixo.png"
-                      alt="Descrição da imagem"
-                      width={30}
-                      height={40} 
-                      className="animate-bounce"
-              />
-              <Link href={'https://api.whatsapp.com/send?phone=5592994916643&text=Ol%C3%A1,%20vim%20pelo%20portf%C3%B3lio!'} target="_Blank"> <Image src="/icons/zap.png" alt="LinkedIn" width={30} height={20} /></Link>
-        </div>
+    <p className="text-center px-1 font-Kanit text-1xl sm:px-2 lg:px-16 lg:text-1xl lg:text-justify">
+      Estou aberto a oportunidades de emprego nas quais posso contribuir, aprender e crescer. Se você tiver uma boa oportunidade que corresponda às minhas habilidades e experiência, não hesite em entrar em contato comigo.
+    </p>
 
-      
-        {/*Skills */}
-        <div id="skills" className="hidden lg:flex pt-16 pb-16 w-full justify-center items-center flex-col gap-2" data-aos='fade-down'>
-        <Image
-                      src="/icons/hacker.svg"
-                      alt="Descrição da imagem"
-                      width={400}
-                      height={40} 
-        />
-        </div>
-        </div>
+   
+
+    <Image
+      src="/icons/seta-para-baixo.png"
+      alt="Descrição da imagem"
+      width={30}
+      height={40}
+      className="animate-bounce"
+    />
+    <Link href={'https://api.whatsapp.com/send?phone=5592994916643&text=Ol%C3%A1,%20vim%20pelo%20portf%C3%B3lio!'} target="_Blank">
+      <Image src="/icons/zap.png" alt="WhatsApp" width={30} height={20} />
+    </Link> 
+  </div>
+
+</div>
       </section>
-      
     <Footer/>
 
     </div>
